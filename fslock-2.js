b3c86794cbe85884da5c7c2ad098176effeef768
K K UPGRADER <57281597+KKUPGRADER@users.noreply.github.com>
2021-08-31 19:45:17 +0530

    Update fslock-2.js

--- fslock-2.js
+++ fslock-2.js
@@ -61,7 +61,7 @@
                           result.innerHTML = '<div class="alert alert-warning text-center">'+config.lang.validtext+'</div>' ;
                         }
                       } else {
-                        result.innerHTML = validurlit(url.value) ? '<div class="alert alert-success text-center">'+config.lang.convertsuccess+'</div><div class="input-group  flex-nowrap mb-3"><span class="input-group-text" id="final-result"><i class="fas fa-copy fa-fw"></i></span><input type="text"  id ="copythis" class="form-control" onfocus="this.select()" onmouseup="return false"  value="'+linknya+'?u='+encodeURIComponent(CryptoJS.AES.encrypt(JSON.stringify(data), keyit))+'" aria-describedby="final-result"/></div>' : '<div class="alert alert-warning text-center">'+config.lang.validtext+'</div>' ;
+                        result.innerHTML = validurlit(url.value) ? '<div class="alert alert-success text-center">'+config.lang.convertsuccess+'</div><div class="input-group  flex-nowrap mb-3"><span class="input-group-text" id="final-result2"><i class="fas fa-copy fa-fw"></i></span><input type="text"  id ="copythis2" class="form-control" onfocus="this.select()" onmouseup="return false"  value="'+linknya+'?u='+encodeURIComponent(CryptoJS.AES.encrypt(JSON.stringify(data), keyit))+'" aria-describedby="final-result2"/></div>' : '<div class="alert alert-warning text-center">'+config.lang.validtext+'</div>' ;
                       }
                     } else {
                       result.innerHTML = '<div class="alert alert-danger text-center">'+config.lang.urlempty+'</div>';
