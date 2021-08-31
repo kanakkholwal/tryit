4cdc195f9769f4bae979fd4bfe39185c7936eaeb
K K UPGRADER <57281597+KKUPGRADER@users.noreply.github.com>
2021-08-31 19:19:50 +0530

    Update fslock-2.js

--- fslock-2.js
+++ fslock-2.js
@@ -6,8 +6,8 @@
 
                   $('#passbtn').on('click', function(e) {
                     var $this = $(this);
-                      if($this.hasClass('btn-dark')){
-                        $this.removeClass('btn-dark').addClass('btn-primary');
+                      if($this.hasClass('btn-outline-dark')){
+                        $this.removeClass('btn-outline-dark').addClass('btn-outline-info');
                         $('#passinput').removeAttr('disabled');
                       } else {
                         $this.removeClass('btn-success').addClass('btn-dark');
@@ -38,7 +38,7 @@
                             dataType: 'json',
                             cache: true,
                             beforeSend: function() {
-                              result.innerHTML = '<div class="text-center"><span class="spinner-border spinner-border-sm text-primary" role="status" aria-hidden="true"></span> Fetch Post</div>' ;
+                              result.innerHTML = '<div class="text-center"><div class="spinner-border text-success" role="status"><span class="visually-hidden">Loading...</span><h4>Fetching Post</h4></div>' ;
                             },
                             success: function(a){
                               var post = a.feed.entry,
@@ -50,13 +50,13 @@
                                   break;
                                 }
                               }
-                              result.innerHTML = '<div class="alert alert-success text-center">'+config.lang.convertsuccess+'</div><div class="input-group mb-0"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-copy fa-fw"></i></span></div><input class="form-control" onfocus="this.select()" onmouseup="return false" style="box-shadow: 0 0 0 0 transparent" value="'+linknya+'?u='+encodeURIComponent(CryptoJS.AES.encrypt(JSON.stringify(data), keyit))+'"/></div>';
+                              result.innerHTML = '<div class="alert alert-success text-center">'+config.lang.convertsuccess+'</div><div class="input-group mb-3"><span class="input-group-text" id="final-result"><i class="fas fa-copy fa-fw"></i></span><input type="text"  id ="copyit" class="form-control" onfocus="this.select()" onmouseup="return false"  value="'+linknya+'?u='+encodeURIComponent(CryptoJS.AES.encrypt(JSON.stringify(data), keyit))+'" aria-describedby="final-result"/></div>';
                               setTimeout(function(){
                                 result.parentNode.style.height = result.offsetHeight+'px';
                               }, 0);
                             }
                           });
-                          // result.innerHTML = '<div class="text-center"><span class="spinner-border spinner-border-sm text-primary" role="status" aria-hidden="true"></span> Fetch Post</div>' ;
+                          // result.innerHTML = '<div class="text-center"><div class="spinner-border text-success" role="status"><span class="visually-hidden">Loading...</span></div> Fetching Post</div>' ;
                         } else {
                           result.innerHTML = '<div class="alert alert-warning text-center">'+config.lang.validtext+'</div>' ;
                         }
